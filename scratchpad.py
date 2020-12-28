54ae851a6ce7a42e018492fa6eab79281f6f845e
VigneshDev1 <vignesh.dev91@gmail.com>
2020-12-27 19:01:37 -0700

    updated

--- scratchpad.py
+++ scratchpad.py
@@ -1,7 +1,14 @@
-import re
-print (5+2)
-str = r"roll"
-checkstr = " this is how we rollor!!"
+import pandas as pd
+dataset = 'https://sololearn.com/uploads/files/titanic.csv'
+# DataFrame : table of data
 
-v = re.findall(str,checkstr)
-print (v[0] + " Hello")
+df = pd.read_csv(dataset)
+print(df.head())  # head() method shows the first 5 rows of dataset
+pd.options.display.max_columns = 6
+# print(df.describe())  # describe() method shows statistical summary (ie. mean, std, min, 25%, 50%, max)
+# print(list(df.columns))  # prints all column headers
+
+getAgeData = df['Age']  # extracts as data series
+getmultiCol = df[['Age', 'Survived', 'Fare']]  # Use double square brackets for multiple columns
+
+print(getmultiCol)
