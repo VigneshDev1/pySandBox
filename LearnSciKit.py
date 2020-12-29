99e5720bcbbed84b4e5f378d8504ead0fb30e0fb
VigneshDev1 <vignesh.dev91@gmail.com>
2020-12-29 14:41:48 -0700

    updated

--- LearnSciKit.py
+++ LearnSciKit.py
@@ -9,21 +9,25 @@ dataset = 'https://sololearn.com/uploads/files/titanic.csv'
 df = pd.read_csv(dataset)
 df['male'] = df['Sex'] == 'male'
 X = df[['Fare', 'Age']].values
+X1 = df[['Pclass', 'male', 'Age', 'Siblings/Spouses', 'Parents/Children', 'Fare']].values
 Y = df['Survived'].values
-print(X)
+print(X1)
 # print(Y)
 model = LReg()
-model.fit(X, Y)
+model.fit(X1, Y)
+print(Y[:5])
+Y_pred = model.predict(X1)
+print(Y_pred[:5])
 
 print(model.coef_, model.intercept_)
 print(model.coef_)
-Y_pred = model.predict(X)
 # print(Y_pred)
 # region .....PLOTTING.....
 from matplotlib import pyplot as plt
-linx = np.linspace(20,110,100)
+
+linx = np.linspace(20, 110, 100)
 print(model.coef_[0][1])
-liny = (model.coef_[0][0]*linx + model.intercept_)/(-1*model.coef_[0][1])
+liny = (model.coef_[0][0] * linx + model.intercept_) / (-1 * model.coef_[0][1])
 plt.scatter(df['Fare'], df['Age'], c=df['Survived'])
 plt.plot(linx, liny, '-r', label='Linear Regression')
 plt.xlabel('Fare')
