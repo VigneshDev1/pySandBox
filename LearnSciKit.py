52a7d95aa6c28e81a0d2f41bb021ebbbf627688e
VigneshDev1 <vignesh.dev91@gmail.com>
2020-12-29 15:00:11 -0700

    updated

--- LearnSciKit.py
+++ LearnSciKit.py
@@ -17,21 +17,23 @@ model = LReg()
 model.fit(X1, Y)
 print(Y[:5])
 Y_pred = model.predict(X1)
-print(Y_pred[:5])
-
+# shape method returns the dim of array (row, col). shape[0] -> rows, shape[1] -> columns
+print(str((Y == Y_pred).sum()) + " of " + str(Y.shape[0]) + " datapoints predicted correctly using the Linear Model")
+# score method gives model's accuracy in % (ie. correct pred/total data)
+print('Model Score: {0}%'.format(round(model.score(X1, Y) * 100, 2)))
 print(model.coef_, model.intercept_)
 print(model.coef_)
 # print(Y_pred)
-# region .....PLOTTING.....
-from matplotlib import pyplot as plt
-
-linx = np.linspace(20, 110, 100)
-print(model.coef_[0][1])
-liny = (model.coef_[0][0] * linx + model.intercept_) / (-1 * model.coef_[0][1])
-plt.scatter(df['Fare'], df['Age'], c=df['Survived'])
-plt.plot(linx, liny, '-r', label='Linear Regression')
-plt.xlabel('Fare')
-plt.ylabel('Age')
-plt.show()
-
-# endregion
+# # region .....PLOTTING.....
+# from matplotlib import pyplot as plt
+#
+# linx = np.linspace(20, 110, 100)
+# print(model.coef_[0][1])
+# liny = (model.coef_[0][0] * linx + model.intercept_) / (-1 * model.coef_[0][1])
+# plt.scatter(df['Fare'], df['Age'], c=df['Survived'])
+# plt.plot(linx, liny, '-r', label='Linear Regression')
+# plt.xlabel('Fare')
+# plt.ylabel('Age')
+# plt.show()
+#
+# # endregion
